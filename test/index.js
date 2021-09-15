5276c3041bed6f0f02b5433e0d48b30d790a0bc2
Carolina S. M <carostmaria@gmail.com>
2021-09-14 22:04:34 -0300

    Add first Node test

---
+++ test/index.js
@@ -0,0 +1,13 @@
+var a = 6
+var b = 4
+var c = a + b
+
+console.log(c)
+
+let num1 = 6
+let num2 = 5
+
+const sumar = (a, b) => a + b 
+
+console.log(`La suma de ${num1} más ${num2} es ${sumar(num1, num2)}`)
+
